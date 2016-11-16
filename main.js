b6e0fbda63dcdbb40a42bbc482c6dd9739e24dbf
JoshuaSeiple <jseiple@bancroftschool.org>
2016-11-16 09:25:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,32 @@ function check(){
 	var question3 = document.quiz.question3.value;
 	var correct = 0;
 
-		if (question1 == 10mm pistol || 10 mm pistol || 10 millimeter pistol) {
+		if (question1 == "10mm pistol") {
 			correct++;
 		}
-		if (question2 == Security Baton) {
+		if (question2 == "Security baton") {
 			correct++;
 		}
-		if(question3 == Radroach){
+		if(question3 == "Radroach"){
 			correct++;
 		}
+		var messages = ["Great job", "Pretty good", "You need to study"]
+var score;
+if (correct < 1) {
+	score = 2;
+}
+
+if (correct > 1 && correct < 3) {
+	score = 1
+}
+
+
+
+
+
+
 
 document.getElementById('afterSubmit').style.visibility = "visible";
-document.getElementById('numberCorrect').innerHTML = "you got" + correct + "correct";
+document.getElementById('numberCorrect').innerHTML = "you got " + correct + " correct.";
 
 }
